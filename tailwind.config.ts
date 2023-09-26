62b902e3183879e8ec341c87ff73db0c051a1dd6
John Leskas <john.leskas@gmail.com>
2023-09-26 19:41:23 +0300

    initial project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        gray: "var(--color-gray)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        tertiary: "var(--color-tertiary)",
+        quaternary: "var(--color-quaternary)",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
+export default config;
