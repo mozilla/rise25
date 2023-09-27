15d1451552f79b809f3539838e10f3aa8cf47dd2
John Leskas <john.leskas@gmail.com>
2023-09-28 02:52:48 +0300

    feat: footer component implementation

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React from "react"
+import Link from 'next/link';
+
+const navigation = [
+    { name: 'Website Privacy Notice', href: 'https://mozilla.org/privacy/websites/' },
+    { name: 'Cookies', href: 'https://www.mozilla.org/privacy/websites/#cookies' },
+    { name: 'Legal', href: 'https://www.mozilla.org/about/legal/terms/mozilla/' },
+]
+
+export const Footer = () => {
+
+    return (
+        <div className="relative py-4 lg:py-6 w-full">
+
+            <div className="container container--xl">
+                <div className="flex flex-col gap-4 lg:gap-6">
+                    <p>Portions of this content are ©1998–2022 by individual mozilla.org contributors. Content available under a Creative Commons license.</p>
+                    <nav className="flex gap-6" role="navigation">
+                        {navigation.map((item) => (
+                            <Link key={item.name} href={item.href} className="underline hover:text-primary">
+                                <span>{item.name}</span>
+                            </Link>
+                        ))}
+                    </nav>
+                </div>
+            </div>
+
+        </div>
+    )
+}
