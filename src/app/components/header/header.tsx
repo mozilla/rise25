21750f0808ddc5c43206e33985bf77277ccd7783
John Leskas <john.leskas@gmail.com>
2023-09-28 02:52:40 +0300

    feat: header component implementation

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React from "react"
+import Link from 'next/link';
+import Image from "next/image";
+
+export const Header = () => {
+
+    return (
+        <div className="sticky top-0 z-50 w-full ">
+
+            <div className="px-4 sm:px-6 container container--xl">
+                <div className="flex items-center justify-between py-6 gap-4">
+                    <Link href="#winners">See the winners</Link>
+                    <div className="flex justify-start">
+                        <Link href="/">
+                            <span className="sr-only">Rise25</span>
+                            <Image
+                                height={70}
+                                width={70}
+                                className="w-auto h-6"
+                                src="/logo.svg"
+                                alt="Rise25"
+                            />
+                        </Link>
+                    </div>
+                    <Link href="/">Celebrate with us</Link>
+                </div>
+            </div>
+
+        </div>
+    )
+}
