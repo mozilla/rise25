cdf3f35b89a02739d5ec1b215f5a186cebce0727
John Leskas <john.leskas@gmail.com>
2023-10-01 19:43:52 +0300

    chore: mobile updates

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -7,9 +7,9 @@ import Image from "next/image";
 export const Header = () => {
 
     return (
-        <header className="sticky top-0 z-50 w-full ">
+        <header className="header sticky top-0 z-50 w-full ">
 
-            <div className="px-4 sm:px-6 container container--xl">
+            <div className="px-4 sm:px-6 container container--2xl">
                 <div className="flex items-center justify-between py-6 gap-4">
                     <Link href="#winners">See the winners</Link>
                     <div className="flex justify-start">
