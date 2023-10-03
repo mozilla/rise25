4d070abc155266ffb823dfa2d988fbb2e02d2ea5
John Leskas <john.leskas@gmail.com>
2023-10-03 20:54:56 +0300

    chore: first working horizontal scroll

--- src/app/components/winners/winners.tsx
+++ src/app/components/winners/winners.tsx
@@ -10,18 +10,20 @@ import { data } from '@/app/lib/data';
 
 gsap.registerPlugin(ScrollTrigger);
 
+const scrollMultiplier = 1.5;
+
 const animateSlider = (enabled: boolean, slider: React.RefObject<HTMLDivElement | null>) => {
     let ctx = gsap.context(() => {
         if (enabled && slider.current) {
             const slideContainer = slider.current;
             const slideItems = gsap.utils.toArray('.card');
 
-            console.log(slideItems.length)
-            console.log(slider.current.offsetWidth);
-            console.log(slideItems[1].offsetWidth);
+            // console.log(slideItems.length)
+            // console.log(slider.current.offsetWidth);
+            // console.log(slideItems[1].offsetWidth);
 
             gsap.to(slideItems, {
-                xPercent: -100 * (slideItems.length + 6), // Calculate total width based on the number of items
+                xPercent: -100 * (slideItems.length * scrollMultiplier), // Calculate total width based on the number of items
                 ease: 'none',
                 scrollTrigger: {
                     markers: true,
@@ -29,6 +31,7 @@ const animateSlider = (enabled: boolean, slider: React.RefObject<HTMLDivElement 
                     pin: true,
                     scrub: true,
                     snap: 1 / (slideItems.length - 1), // Calculate snap points
+                    start: 'top 112',
                     end: () => `+=${slideContainer.offsetWidth}`, // Adjust the end value based on container width
                 },
             });
@@ -45,65 +48,66 @@ export const Winners = ({ enabled }: { enabled: boolean }) => {
         animateSlider(enabled, slider);
     }, [enabled]);
 
+    const scrollToCard = (index) => {
+
+    }
+
     if (!enabled) {
         return <></>
     }
 
     return (
-        <>
-
-            <div id="winners" className={` text-white section section--xl`}>
-                <div className="container container--2xl">
-                    <div className="group-navigation">
-                        {winnerGroups.map((group, groupIndex) => (
-                            <React.Fragment key={groupIndex}>
-                                <div className="group-navigation-item" key={groupIndex}>
-                                    <Link className="group-card-link" href={`#${group.group.slug}`}>
-                                        {group.group.name}
-                                    </Link>
-                                </div>
-                                {groupIndex < winnerGroups.length - 1 && <span> / </span>}
-                            </React.Fragment>
-                        ))}
-                    </div>
-
-                    <div className="winners" ref={slider}>
-                        {winnerGroups.map((group, groupIndex) => (
-                            <div key={groupIndex} className="winner-group">
-                                {group.group && (
-                                    <div id={group.group.slug} className="group-card card">
-                                        <div className={`group-card-inner`}>
-                                            <h3 className={`group-card-title`}>{group.group.name} <ArrowRight className={`h-12 text-[140%]`} />
-                                            </h3>
-                                            <p className="text-lg">{group.group.description}</p>
-                                        </div>
-                                        <Image className={`z-0`} src={group.group.imgSrc} layout="fill" objectFit="cover" alt={group.group.name} />
-                                    </div>
-                                )}
-                                {group.winners.map((winner, winnerIndex) => (
-                                    <div key={winnerIndex} className="winner card group">
-                                        <div className="winner-name">{winner.title}</div>
-                                        <div className="winner-info">
-                                            <div className="winner-social">
-                                                {winner.socialMedia.map((social, socialIndex) => (
-                                                    <a className="winner-social-link" key={socialIndex} href={social.url} target="_blank" rel="noopener noreferrer">
-                                                        {social.platform === "Instagram" && <Instagram />}
-                                                        {social.platform === "Twitter" && <Twitter />}
-                                                        {social.platform === "YouTube" && <Youtube />}
-                                                        {social.platform === "Web" && <Web />}
-                                                    </a>
-                                                ))}
-                                            </div>
-                                            <div className="winner-bio" dangerouslySetInnerHTML={{ __html: winner.bio }} />
-                                        </div>
-                                        <Image src={winner.imgSrc} layout="fill" objectFit="cover" alt={winner.title} />
-                                    </div>
-                                ))}
+        <div id="winners" className={` text-white section section--xl`} ref={slider}>
+            <div className="container container--2xl">
+                <div className="group-navigation">
+                    {winnerGroups.map((group, groupIndex) => (
+                        <React.Fragment key={groupIndex}>
+                            <div className="group-navigation-item" key={groupIndex}>
+                                <Link className="group-card-link" href={`#${group.group.slug}`}>
+                                    {group.group.name}
+                                </Link>
                             </div>
-                        ))}
-                    </div>
+                            {groupIndex < winnerGroups.length - 1 && <span> / </span>}
+                        </React.Fragment>
+                    ))}
+                </div>
+
+                <div className="winners" >
+                    {winnerGroups.map((group, groupIndex) => (
+                        <div key={groupIndex} className="winner-group">
+                            {group.group && (
+                                <div id={group.group.slug} className="group-card card">
+                                    <div className={`group-card-inner`}>
+                                        <h3 className={`group-card-title`}>{group.group.name} <ArrowRight className={`h-12 text-[140%]`} />
+                                        </h3>
+                                        <p className="text-lg">{group.group.description}</p>
+                                    </div>
+                                    <Image className={`z-0`} src={group.group.imgSrc} layout="fill" objectFit="cover" alt={group.group.name} />
+                                </div>
+                            )}
+                            {group.winners.map((winner, winnerIndex) => (
+                                <div key={winnerIndex} className="winner card group">
+                                    <div className="winner-name">{winner.title}</div>
+                                    <div className="winner-info">
+                                        <div className="winner-social">
+                                            {winner.socialMedia.map((social, socialIndex) => (
+                                                <a className="winner-social-link" key={socialIndex} href={social.url} target="_blank" rel="noopener noreferrer">
+                                                    {social.platform === "Instagram" && <Instagram />}
+                                                    {social.platform === "Twitter" && <Twitter />}
+                                                    {social.platform === "YouTube" && <Youtube />}
+                                                    {social.platform === "Web" && <Web />}
+                                                </a>
+                                            ))}
+                                        </div>
+                                        <div className="winner-bio" dangerouslySetInnerHTML={{ __html: winner.bio }} />
+                                    </div>
+                                    <Image src={winner.imgSrc} layout="fill" objectFit="cover" alt={winner.title} />
+                                </div>
+                            ))}
+                        </div>
+                    ))}
                 </div>
             </div>
-        </>
+        </div>
     )
 }
