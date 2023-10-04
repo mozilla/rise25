7634eba55db8b61ef8008db12941650a62146a2d
Dan Brown <slightlyoffbeat@gmail.com>
2023-10-04 00:04:50 -0700

    redirecting to blog

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:slug*',
+        destination: 'https://blog.mozilla.org/en/mozilla/rise-25-winners/?source=web-redirect',
+        permanent: false,
+        basePath: false
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
