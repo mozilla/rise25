caf1a459ceebac5974504d597dc6ed5b5a0ed083
Craig Cook <craigcook@users.noreply.github.com>
2024-06-20 13:02:37 -0700

    New site for 2024

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rise25 2024 Honorees
+Announcing the Mozilla Rise25 honorees for 2024.
+https://rise25.mozilla.org/
+
+## Local Development
+```bash
+# install dependencies if you haven't already
+npm install
+
+# run dev server and check your console for the local URL
+npm start
+```
---
+++ media/js/site.js
@@ -0,0 +1,15 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+(function () {
+    'use strict';
+
+    var html = document.documentElement;
+
+    // Add class to reflect javascript availability for CSS
+    html.className = html.className.replace(/\bno-js\b/, 'js');
+
+})()
